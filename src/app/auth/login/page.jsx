9fde07186f47425bf2128f3e783bf94b39813766
Yuuki-Ishino <yyuuuukkii0914@gmail.com>
2025-10-17 19:58:02 +0900

    change: redirectTo

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "/auth/callback",
+        redirectTo: "http://bbooth.vercel.app/auth/callback",
       },
     });
   };
